0caf8aadd6b0db103fc9eec82c6ab296aaf16e08
lbako801 <lbako98@gmail.com>
2022-10-02 20:44:07 -0600

    Added character length limitations and prompts.

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// This function is what generates the password for the 'writePassword' function.
+function generatePassword() {
+  var answer = window.prompt("How many characters (8-128) will the password be?")
+
+  var characterlength = parseInt(answer)
+
+  if (isNaN(characterlength)) {
+    window.alert("Amount of characters must be a NUMBER between 8-128 characters long.")
+    return
+  }
+
+  if (characterlength < 8 || characterlength > 128) {
+    window.alert("Amount of characters must be a NUMBER between 8-128 characters long.")
+    return
+  }
+
+  
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
